cf096a29393616409e399d88a5e97d4b99616abb
lukaskreibig <lukatze@posteo.de>
2021-04-19 12:08:38 +0200

    new changes, new components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import axios from 'axios';
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import { decode } from 'html-entities';
 
 function App() {
 
+  
   const [quizArray, setQuizArray] = useState([]);
 
  const quizFetch = () => {
@@ -12,20 +14,27 @@ function App() {
    .then((res) => setQuizArray(res.data.results));
   };
 
+
   useEffect(quizFetch, [])
 
-return (
-    quizArray.length > 0 && (
+
+return quizArray.length > 0 ? (
       <div className='App'>
-        Question
-        <h2> {quizArray[0].question} </h2>
+        <h2>  {quizArray[0].question} </h2>
         <button> {quizArray[0].correct_answer} </button>
-        {quizArray[0].incorrect_answers.map((answer, index) => (
-          <button> {answer[index]} </button>
+        {quizArray[0].incorrect_answers.map((answer) => (
+          <button> {answer} </button>
         ))}
       </div>
-    )
-  );
+    ) : (
+      <div className='App'>
+      <h2> ...Loading QUIZ...</h2>
+      </div>
+    );
 }
 
 export default App;
+
+  //.replace(/&quot;/g, '\"').replace(/;&#039;/g,"'").replace(/&#039;/g,"'").replace(/&rsquo;/g,"'")
+  //   decode('&quot;');
+  //const regex = / /g
