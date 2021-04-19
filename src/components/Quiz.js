7195b4cfde67149557e0400ad12a7bb57fc1e9ab
lukaskreibig <lukatze@posteo.de>
2021-04-19 23:25:49 +0200

    randomize answers, render answers through map

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,30 +2,43 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 function Quiz() {
-    const [quizArray, setQuizArray] = useState([]);
+  const [quizArray, setQuizArray] = useState([]);
 
-    const quizFetch = () => {
-        axios
-            .get("https://opentdb.com/api.php?amount=10&type=multiple")
-            .then((res) => setQuizArray(res.data.results));
-    };
+  const quizFetch = () => {
+    axios
+      .get("https://opentdb.com/api.php?amount=10&type=multiple")
+      .then((res) => setQuizArray(res.data.results));
+  };
 
-    useEffect(quizFetch, []);
+  useEffect(quizFetch, []);
 
-    return quizArray.length > 0 ? (
-        <div className="App">
-            <h2> {quizArray[0].question} </h2>
-            <button> {quizArray[0].correct_answer} </button>
-            {quizArray[0].incorrect_answers.map((answer) => (
-                <button> {answer} </button>
-            ))}
-        </div>
-    ) : (
-        <div className="App">
-            <h2> ...Loading QUIZ...</h2>
-            <div></div>
-        </div>
-    );
+  let quizAnswers = [
+    quizArray[0].correct_answer.push(quizArray[0].incorrect_answers),
+    console.log(quizAnswers),
+  ];
+
+  let shuffledAnswers = quizAnswers
+    .map((a) => ({ sort: Math.random(), value: a }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value);
+  console.log(shuffledAnswers);
+
+  return quizArray.length > 0 ? (
+    <div>
+      <h2> {quizArray[0].question} </h2>
+
+      {shuffledAnswers.map((answers) => (
+        <button> {answers} </button>
+      ))}
+
+      {/* Make an array of answers shuffled and map over them to render the button */}
+    </div>
+  ) : (
+    <div className="App">
+      <h2> ...Loading QUIZ...</h2>
+      <div></div>
+    </div>
+  );
 }
 
 //https://opentdb.com/api.php?amount=10
