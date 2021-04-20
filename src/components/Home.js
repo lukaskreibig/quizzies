e07a557f55e4039d02f7dcf72b9be4e04149e9f4
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-04-20 19:53:32 +0200

    add click function to start button; add redirection to InputSelect on click

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import "./Home.css";
 import StartBtn from "./StartBtn";
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
         <div className="Homebody">
             <h1>Welcome to Trivia Night</h1>
             <h2>Enter the Game</h2>
-            <StartBtn />
+            <Link to="/inputselect">
+                <StartBtn />
+            </Link>
         </div>
     );
 }
