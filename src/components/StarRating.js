aed8b0255f97ad7c237e13a1618c3ddf71e0905d
Isabel <isabel.mehlmann@gmail.com>
2021-04-20 14:25:51 +0200

    added feedback stars in StarRating.js

---
+++ src/components/StarRating.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+const StarRating = () => {
+    const [rating, setRating] = useState(null);
+    const [hover, setHover] = useState(null);
+    return (
+    <div>
+        {[...Array(5)].map((star, i) => {
+            const ratingValue = i + 1;
+            
+
+
+            return (
+            <label>
+                <input 
+                type="radio" 
+                name="rating" 
+                value={ratingValue} 
+                onClick={()=> setRating(ratingValue)}
+                
+                />
+
+                <FaStar 
+                className="star" 
+                color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9" } 
+                size={50} 
+                onMouseEnter={() => setHover(ratingValue)}
+                onMouseLeave={() => setHover(null)}
+                />
+            </label>
+            );
+        })}
+        <p>Your rating is {rating}.</p>
+    </div>
+    );
+};
+
+
+export default StarRating; 
