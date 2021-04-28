35ecd8881abc14f54b44cc928ea18ad03b90bccf
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-04-28 10:59:12 +0200

    make the answers show (almost) correctly

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,88 +1,107 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Quizz.css";
 
 function Quiz({ quizArray }) {
-  // const [quizArray, setQuizArray] = useState([]);
-  // const quizFetch = () => {
-  //     axios
-  //         .get("https://opentdb.com/api.php?amount=10&type=multiple")
-  //         .then((res) => setQuizArray(res.data.results));
-  // };
-  // useEffect(quizFetch, []);
+    //Set status to current question
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+    const [score, setScore] = useState(0);
+    const [answers, setAnswers] = useState([]);
 
-  //Set status to current question
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [showScore, setShowScore] = useState(false);
-  const [score, setScore] = useState(0);
-  const [answers, setAnswers] = useState([]);
-
-  //iterates through all the answers, randomizes them and puts them in the state "answers"
-  for (let i = 0; i < quizArray.length; i++) {
-    answers.push(
-      [quizArray[i].correct_answer, ...quizArray[i].incorrect_answers]
-        .map((a) => ({ sort: Math.random(), value: a }))
-        .sort((a, b) => a.sort - b.sort)
-        .map((a) => a.value)
-        .map((answer) => ({ answerText: answer, isCorrect: true }))
-      // if answer === quizArray[i].correct_answer
-    );
-  }
-  console.log(answers);
-
-  //updating score according to answer selected
-  const handleAnswerOptionClick = (isCorrect) => {
-    if (isCorrect) {
-      setScore(score + 1);
+    //iterates through all the answers, randomizes them and puts them in the state "answers"
+    for (let i = 0; i < quizArray.length; i++) {
+        answers.push(
+            [quizArray[i].correct_answer, ...quizArray[i].incorrect_answers]
+                .map((a) => ({ sort: Math.random(), value: a }))
+                .sort((a, b) => a.sort - b.sort)
+                .map((a) => a.value)
+                .map((answer) => ({
+                    answerText: answer,
+                    isCorrect:
+                        answer === quizArray[i].correct_answer ? true : false,
+                }))
+            // if answer === quizArray[i].correct_answer
+        );
     }
-    const nextQuestion = currentQuestion + 1;
-    if (nextQuestion < quizArray.length) {
-      setCurrentQuestion(nextQuestion);
-    } else {
-      setShowScore(true);
-    }
-  };
+    console.log(currentQuestion);
+    console.log(answers);
 
-  return quizArray.length > 0 ? (
-    <div className="App">
-      {showScore ? (
-        <div className="score-section">
-          You scored {score} out of {quizArray.length}
+    // let shuffledAnswers = questions
+    //   .map((a) => ({ sort: Math.random(), value: a }))
+    //   .sort((a, b) => a.sort - b.sort)
+    //   .map((a) => a.value);
+    // console.log(shuffledAnswers, "shuffled");
+
+    //updating score according to answer selected
+    const handleAnswerOptionClick = (isCorrect) => {
+        if (isCorrect) {
+            setScore(score + 1);
+        }
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < quizArray.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setShowScore(true);
+        }
+    };
+
+    return quizArray.length > 0 ? (
+        <div className="App">
+            {showScore ? (
+                <div className="score-section">
+                    You scored {score} out of {quizArray.length}
+                </div>
+            ) : (
+                <>
+                    {/*display current question*/}
+                    <div className="question-section">
+                        <h1 className="question-text">
+                            {quizArray[currentQuestion].question}
+                        </h1>
+                    </div>
+                    {/*display list of answers to the current question*/}
+                    <div className="answer-section">
+                        {/*List of answers*/}
+
+                        {answers[currentQuestion].map((answerOption) => (
+                            <button
+                                onClick={() =>
+                                    handleAnswerOptionClick(
+                                        answerOption.isCorrect
+                                    )
+                                }
+                            >
+                                {answers.answerText}
+                            </button>
+                        ))}
+
+                        {/*{answers.map((answer) => (
+                        <button
+                            className="answer-btn"
+                            onClick={() =>
+                                handleAnswerOptionClick(answer.isCorrect)
+                            }
+                        >
+                            {answer}
+                        </button>
+                        ))}/*}
+                        {/*display question number out of 10*/}
+                        <div className="question-count">
+                            <h2>
+                                Question {currentQuestion + 1}/
+                                {quizArray.length}
+                            </h2>
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
-      ) : (
-        <>
-          {/*display current question*/}
-          <div className="question-section">
-            <h1 className="question-text">
-              {quizArray[currentQuestion].question}
-            </h1>
-          </div>
-          {/*display list of answers to the current question*/}
-          <div className="answer-section">
-            {/*List of answers*/}
-            {quizArray[currentQuestion].incorrect_answers.map((answer) => (
-              <button
-                className="answer-btn"
-                onClick={() => handleAnswerOptionClick(answer.isCorrect)}
-              >
-                {answer}
-              </button>
-            ))}
-            {/*display question number out of 10*/}
-            <div className="question-count">
-              <h2>
-                Question {currentQuestion + 1}/{quizArray.length}
-              </h2>
-            </div>
-          </div>
-        </>
-      )}
-    </div>
-  ) : (
-    <div className="App">
-      <h2> ...Loading QUIZ...</h2>
-      <div></div>
-    </div>
-  );
+    ) : (
+        <div className="App">
+            <h2> ...Loading QUIZ...</h2>
+            <div></div>
+        </div>
+    );
 }
 
 export default Quiz;
