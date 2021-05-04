5b4d81bda9c21ba67fdf62225790b1a0305d099d
lukaskreibig <lukatze@posteo.de>
2021-05-04 22:54:10 +0200

    integrate timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,20 +1,20 @@
-
-import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 const Timer = ({ key }) => (
   <CountdownCircleTimer
-  key={key}
-  isPlaying
-  duration={10}
-  colors={[['#32b8ca', 0.33],
-           ['#e053a8', 0.33],
-           ['#e1af4f', 0.33],
-]}
-  onComplete={() => [true, 1000]}
-  size={80}
->
-{({ remainingTime }) => remainingTime}
-</CountdownCircleTimer>
-)
+    key={key}
+    isPlaying
+    duration={1}
+    colors={[
+      ["#32b8ca", 0.33],
+      ["#e053a8", 0.33],
+      ["#e1af4f", 0.33],
+    ]}
+    onComplete={() => console.log("Its Over!")}
+    size={80}
+  >
+    {({ remainingTime }) => remainingTime}
+  </CountdownCircleTimer>
+);
 
-export default Timer; 
+export default Timer;
