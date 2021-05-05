6dfcb5d5c1804a28103bc917da7cc04c4d8ae296
lukaskreibig <lukatze@posteo.de>
2021-05-05 10:15:59 +0200

    change timer to 15 seconds, coto light red

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,11 +4,11 @@ const Timer = ({ key, timesUp }) => (
   <CountdownCircleTimer
     key={key}
     isPlaying
-    duration={12}
+    duration={15}
     colors={[
       ["#1a6340", 0.33],
       ["#FFFF00", 0.33],
-      ["#BE1600", 0.33],
+      ["#ff0000", 0.33],
     ]}
     onComplete={() => timesUp()}
     size={80}
