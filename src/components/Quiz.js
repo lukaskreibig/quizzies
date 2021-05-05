ef79ffe422440c8c44ee6c4f33713ae1fbdc512a
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-05-05 13:09:01 +0300

    css for result page desktop version

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -94,67 +94,74 @@ function Quiz({ quizArray }) {
         <div className="App">
             {showScore ? (
                 <>
-                    <h1 id="result-title">TRIVIA NIGHT</h1>
-                    <div className="result-structure">
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td colspan="1">
-                                        <div className="pink-star">★</div>
-                                        <div className="blue-star">★</div>
-                                        <div className="pink-star">★</div>
-                                    </td>
-                                    <td colspan="4">
-                                        <div className="score-section">
-                                            <h2>Quiz Result</h2>
+                    <div className="result-body">
+                        <div className="result-structure">
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td colspan="1">
+                                            <div className="pink-star">★</div>
+                                            <div className="blue-star">★</div>
+                                            <div className="pink-star">★</div>
+                                        </td>
+                                        <td
+                                            colspan="4"
+                                            className="middle-column"
+                                        >
+                                            <div className="score-section">
+                                                <h2>Quiz Result</h2>
+                                                <h3>You made it to</h3>
+                                                <div className="master-level">
+                                                    {master()}
+                                                </div>
+                                            </div>
                                             <h3>You scored</h3>
                                             <div className="final-score-height">
                                                 <h1 className="final-score">
                                                     {score} / {quizArray.length}
                                                 </h1>
                                             </div>
-                                            <h3>You made it to</h3>
-                                            <div className="master-level">
-                                                {master()}
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td colspan="1">
-                                        <div className="blue-star">★</div>
-                                        <div className="pink-star">★</div>
-                                        <div className="blue-star">★</div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div className="after-quiz">
-                        <div className="after-quiz-options">
-                            <h2>Did you like it?</h2>
-                            <Link className="link-feedback-btn" to="/contact">
-                                <button className="feedback-btn">
-                                    Leave your Feedback
-                                </button>
-                            </Link>
+                                        </td>
+                                        <td colspan="1">
+                                            <div className="blue-star">★</div>
+                                            <div className="pink-star">★</div>
+                                            <div className="blue-star">★</div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
-                        <div className="after-quiz-options">
-                            <h2>Play again</h2>
-                            <div className="img-container-small">
-                                <img
-                                    className="startbtnsmall"
-                                    src="https://i.ibb.co/ZKNdzPR/start-button-without-play.png"
-                                    alt="start button"
-                                />
+                        <div className="after-quiz">
+                            <div className="after-quiz-options">
+                                <h3>Did you like it?</h3>
                                 <Link
-                                    className="link-btn-small"
-                                    to="/inputselect"
+                                    className="link-feedback-btn"
+                                    to="/contact"
                                 >
-                                    <img
-                                        className="playbtnsmall"
-                                        src="https://i.ibb.co/M5fhCSf/start-button-play-only.png"
-                                        alt="play button"
-                                    />
+                                    <button className="feedback-btn">
+                                        Leave your Feedback
+                                    </button>
                                 </Link>
+                            </div>
+                            <div className="after-quiz-options">
+                                <h3>Play again</h3>
+                                <div className="img-container-small">
+                                    <img
+                                        className="startbtnsmall"
+                                        src="https://i.ibb.co/ZKNdzPR/start-button-without-play.png"
+                                        alt="start button"
+                                    />
+                                    <Link
+                                        className="link-btn-small"
+                                        to="/inputselect"
+                                    >
+                                        <img
+                                            className="playbtnsmall"
+                                            src="https://i.ibb.co/M5fhCSf/start-button-play-only.png"
+                                            alt="play button"
+                                        />
+                                    </Link>
+                                </div>
                             </div>
                         </div>
                     </div>
