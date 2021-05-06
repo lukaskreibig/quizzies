72d294f4c91c537ed09421313b83df8fc32fea7b
lukaskreibig <lukatze@posteo.de>
2021-05-06 17:48:45 +0200

    resolve error

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -1,28 +1,33 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
-function TeamMember({memberInfo}){
-  return(
-      <div className="cardTeam">
-        <img className="avatar" src={memberInfo.photo} alt= ""/>
-        <div className="cardContent">
-          <div className="texttitle">{memberInfo.name}</div>
-          <div className="text2">{memberInfo.description}</div>
-          <a target="_blank" href={memberInfo.linkedIcon}
-            className="logosAbout">
-            <FontAwesomeIcon icon={faLinkedin} size="2x" />
-          </a>
-            <a target="_blank" href={memberInfo.gitHub}
-            className="logosAbout">
-            <FontAwesomeIcon icon={faGithub} size="2x" />
-          </a>       
-        </div>
+function TeamMember({ memberInfo }) {
+  return (
+    <div className="cardTeam">
+      <img className="avatar" src={memberInfo.photo} alt="" />
+      <div className="cardContent">
+        <div className="texttitle">{memberInfo.name}</div>
+        <div className="text2">{memberInfo.description}</div>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={memberInfo.linkedIcon}
+          className="logosAbout"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={memberInfo.gitHub}
+          className="logosAbout"
+        >
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+        </a>
       </div>
-  )
+    </div>
+  );
 }
-  
-export default TeamMember
 
-        
-
+export default TeamMember;
