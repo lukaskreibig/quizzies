a401c77080895bb98638fb75e726e2aee420f21f
makrmeen <makrmeen@gmail.com>
2021-05-06 15:42:19 +0200

    style about us prtiier better

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {  faLinkedin} from "@fortawesome/free-brands-svg-icons";
-
-import "./about.css";
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
 
 function TeamMember({memberInfo}){
   return(
-      <div className="card">
+      <div className="cardTeam">
         <img className="avatar" src={memberInfo.photo} alt= ""/>
         <div className="cardContent">
-          <h2>{memberInfo.name}</h2>
-          <p>{memberInfo.description}</p>
+          <div className="texttitle">{memberInfo.name}</div>
+          <div className="text2">{memberInfo.description}</div>
           <a target="_blank" href={memberInfo.linkedIcon}
-            className="linkedin">
+            className="logosAbout">
             <FontAwesomeIcon icon={faLinkedin} size="2x" />
           </a>
+            <a target="_blank" href={memberInfo.gitHub}
+            className="logosAbout">
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>       
         </div>
       </div>
   )
