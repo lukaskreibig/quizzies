1beaa9b25e0b399c9d2c1acb9c4fa99b7236be7f
makrmeen <makrmeen@gmail.com>
2021-05-06 12:50:40 +0200

    about almost working

--- src/components/About.js
+++ src/components/About.js
@@ -19,13 +19,13 @@ const teamMembers = [
       name: "Lukas",
       photo: "https://media-exp1.licdn.com/dms/image/C5603AQFlXYSL_ic5aA/profile-displayphoto-shrink_800_800/0/1615720340726?e=1625702400&v=beta&t=rLmAyLSKvyJdod5bEcDQeBqXVZZQaiMUXg_HvsNRMkE",
       description: "Our artist at storytelling with fantastic photographs has travelled around the world learning about it just to captur seconds but with the amazing challenge of showing the views chapters of the story behind it. He is a cat and nature enthusiast.",
-      linkedIn:  "https://www.linkedin.com/in/lukas-kreibig-0513ab208/",
+      linkedIcon:  "https://www.linkedin.com/in/lukas-kreibig-0513ab208/",
     },
     { 
     name: "Carmen",
     photo :   "https://media-exp1.licdn.com/dms/image/C4D03AQF_mM3xUhIWPQ/profile-displayphoto-shrink_400_400/0/1558116558096?e=1625702400&v=beta&t=CtCb4PbBNyDeCmXnu_LjSYG0zOvK4bH11OPCk_F2dgM",
     description: "Former teacher so she thrives is sharing knowledge and knows it's crucial to practice it. Was born in Mexico lived in Canada and now adapting to Paris",
-    linkedIn: "https://www.linkedin.com/in/makarmeen/"
+    linkedIcon: "https://www.linkedin.com/in/makarmeen/"
   }
   ];
 
@@ -33,11 +33,15 @@ const teamMembers = [
 
 const About = () => {
     return (
-      <div className="ourHistory">
-        <h1> Our Story </h1>
-        <p> We wanted to create an interactive situation where users could learn and practice knowledge from different topics. Having fun while being challenged. For those routine moments of the day.  </p>
-        {teamMembers.map((member)=> <TeamMember memberInfo={member}/>)}
-      </div>
+      <>
+        <div className="ourHistory">
+          <h1> Our Story </h1>
+          <p> We wanted to create an interactive situation where users could learn and practice knowledge from different topics. Having fun while being challenged. For those routine moments of the day.  </p>
+        </div> 
+        <div className="container">
+          {teamMembers.map((member)=> <TeamMember memberInfo={member}/>)}
+        </div>
+      </>
     )
 };
 
--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -1,21 +1,24 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faLinkedin} from "@fortawesome/free-brands-svg-icons";
 
 import "./about.css";
 
 function TeamMember({memberInfo}){
-    return (
-    <div className="card">
-      <img className="avatar" src={memberInfo.photo} alt= ""/>
-      <div className="cardContent">
-      <h2>        {memberInfo.name}          </h2>
-     <p>        {memberInfo.description}     </p>
- 
- <a href="#" title="LinkedIn" class="btn btn-linkedin btn-lg"><i class="fa fa-linkedin fa-fw"></i> </a>
- </div>
+  return(
+      <div className="card">
+        <img className="avatar" src={memberInfo.photo} alt= ""/>
+        <div className="cardContent">
+          <h2>{memberInfo.name}</h2>
+          <p>{memberInfo.description}</p>
+          <a target="_blank" href={memberInfo.linkedIcon}
+            className="linkedin">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+        </div>
       </div>
-      
-    )
-  
-  }
+  )
+}
   
 export default TeamMember
 
